0be4bb79b0785dfef15693a3d83612739f5cecf2
Ronan Henry <henry.ronan.94@gmail.com>
2017-05-17 10:12:02 +0200

    Merge with branch ronan

---
+++ Models/Asset.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace Map.Models
+{
+    /// <summary>
+    /// Represents an asset of the map.
+    /// </summary>
+    public class Asset
+    {
+        #region Properties
+
+        /// <summary>
+        /// Asset's location on the storage drive.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Asset's pixel data with it's attributes.
+        /// </summary>
+        public Image Picture { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Asset initialization.
+        /// </summary>
+        /// <param name="location">The asset's location on the storage drive.</param>
+        public Asset(string location)
+        {
+            Location = location;
+            Picture = new Bitmap(Location);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns the asset's name based on it's location.
+        /// Takes an optional boolean parameter to include the extension in the name (default is false).
+        /// </summary>
+        /// <param name="withExtension">A boolean value to get the extension in the name.</param>
+        /// <returns>The name of the asset.</returns>
+        public string GetName(bool withExtension = false)
+        {
+            return withExtension ? Path.GetFileName(Location) : Path.GetFileNameWithoutExtension(Location);
+        }
+
+        /// <summary>
+        /// Returns the full location path of the asset on the storage drive.
+        /// </summary>
+        /// <returns>The full path to the asset.</returns>
+        public override string ToString()
+        {
+            return Location;
+        }
+
+        #endregion
+    }
+}
---
+++ Models/Cell.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Map.Models
+{
+    /// <summary>
+    /// Represents a cell of the map.
+    /// </summary>
+    public class Cell
+    {
+        #region Properties
+
+        /// <summary>
+        /// Cell's position on the map.
+        /// </summary>
+        public Point Position { get; set; }
+
+        /// <summary>
+        /// Collection of assets inside the cell.
+        /// </summary>
+        public IList<Asset> Assets { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Cell initialization.
+        /// </summary>
+        public Cell()
+        {
+            Assets = new List<Asset>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Returns a string identifying the cell in the map.
+        /// </summary>
+        /// <returns>A string with the cell's coordinates.</returns>
+        public override string ToString()
+        {
+            return string.Format($"Cell [{Position.X}, {Position.Y}]");
+        }
+
+        #endregion
+    }
+}
